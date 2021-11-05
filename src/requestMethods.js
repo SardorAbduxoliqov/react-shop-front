b2b51dbc2509a817e78f964010a346b455c0339d
SardorAbduxoliqov <sardornewgithub@mail.ru>
2021-11-05 15:22:37 +0500

    token added

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://shopalloma.herokuapp.com/api/";
+const BASE_URL = "https://shopalloma.herokuapp.com/api/";
 const TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNzI5M2YxMDE3MDc3OTljNjdiNGNhOSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2MzQ5ODYwODksImV4cCI6MTYzNTI0NTI4OX0.3e4uV-rCDTADyaIMJf8gq9dm2_xo0Dl2pxQWidYOfeg" || JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
   .currentUser.accessToken;
   
