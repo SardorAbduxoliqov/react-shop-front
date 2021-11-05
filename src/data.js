10c5fbb093b7b99830e4f215043cf43935e81a74
SardorAbduxoliqov <sardornewgithub@mail.ru>
2021-11-05 14:19:38 +0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+ {
+  id: 1,
+  img: "https://images.pexels.com/photos/1537671/pexels-photo-1537671.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  title: "Summer sale",
+  desc: "This is a baby to wear our product",
+  bg: "f5fafd"
+ },
+ {
+  id: 2,
+  img: "https://images.pexels.com/photos/3771605/pexels-photo-3771605.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  title: "Autumn sale",
+  desc: "Two baby wear yellow cloth",
+  bg: "fcf1ed"
+ },
+ {
+  id: 3,
+  img: "https://images.pexels.com/photos/5981113/pexels-photo-5981113.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  title: "Kids sale",
+  desc: "To sell books and nature",
+  bg: "fbf0f4"
+ }
+]
+
+export const categories = [
+ {
+  id: 1,
+  img: "https://images.pexels.com/photos/6618562/pexels-photo-6618562.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  title: "Glasses style",
+  cat: "woman"
+ },
+ {
+  id: 2,
+  img: "https://images.pexels.com/photos/6624333/pexels-photo-6624333.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  title: "Kids style",
+  cat: "man"
+ },
+ {
+  id: 3,
+  img: "https://images.pexels.com/photos/1464625/pexels-photo-1464625.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  title: "Shoes style ",
+  cat: "t-shirt"
+ }
+]
+
+export const popularProducts = [
+ {
+  id: 1,
+  img: "https://images.pexels.com/photos/273930/pexels-photo-273930.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+ },
+ {
+  id: 2,
+  img: "https://images.pexels.com/photos/5496580/pexels-photo-5496580.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+ },
+ {
+  id: 3,
+  img: "https://images.pexels.com/photos/6046183/pexels-photo-6046183.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+ },
+ {
+  id: 4,
+  img: "https://images.pexels.com/photos/4695177/pexels-photo-4695177.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+ },
+ {
+  id: 5,
+  img: "https://images.pexels.com/photos/6045019/pexels-photo-6045019.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+ },
+ {
+  id: 6,
+  img: "https://images.pexels.com/photos/1464624/pexels-photo-1464624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+ },
+ {
+  id: 7,
+  img: "https://images.pexels.com/photos/4464820/pexels-photo-4464820.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+ },
+ {
+  id: 8,
+  img: "https://images.pexels.com/photos/1537671/pexels-photo-1537671.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+ }
+]
